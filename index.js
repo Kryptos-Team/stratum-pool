5a4b1a233201633bf0bb0d9320a26a854834b162
Abhimanyu <desk.abhimanyu@gmail.com>
2020-01-25 17:30:54 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var net = require('net');
+var events = require('events');
+
+//Gives us global access to everything we need for each hashing algorithm
+require('./src/algoProperties.js');
+
+var pool = require('./src/pool.js');
+
+exports.daemon = require('./src/daemon.js');
+exports.varDiff = require('./src/varDiff.js');
+
+
+exports.createPool = function (poolOptions, authorizeFn) {
+    var newPool = new pool(poolOptions, authorizeFn);
+    return newPool;
+};
